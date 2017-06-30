c0ccfe98e66f2ebcc58cb77fdfb0bbdabecb6140
shavit <shavit@users.noreply.github.com>
2017-06-30 11:17:57 -0400

    🛍

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.conf.urls import include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^login/$', views.user_login, name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html')),
     url(r'^logout/$', views.user_logout, name='logout'),
-    # url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
-    # url(r'^logged_out/$', views.user_logged_out, name='logged_out'),
+    url(r'^change-password/$', auth_views.PasswordChangeView.as_view()),
+    url(r'^change-password/done/$', auth_views.PasswordChangeDoneView.as_view()),
+    url(r'^reset-password/$', auth_views.PasswordResetView.as_view()),
+    url(r'^reset-password/done/$', auth_views.PasswordResetDoneView.as_view()),
     url(r'^$', views.dashboard, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .forms import LoginForm
