7aa0ff8e0273a44ff38a03f24ae4aed1e0035d9b
shavit <shavit@users.noreply.github.com>
2017-07-01 20:13:53 -0400

    🛍

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,8 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page,
+        'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
