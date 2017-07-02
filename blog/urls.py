0704f545307771e8a90d0c02555d7541dcec4831
shavit <shavit@users.noreply.github.com>
2017-07-02 11:38:21 -0400

    🛍

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap
+}
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -10,4 +16,8 @@ urlpatterns = [
         views.post_detail,
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    url(r'^sitemap\.xml$',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
--- miley/settings.py
+++ miley/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'easy_thumbnails',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'taggit',
     'miley',
     'blog',
