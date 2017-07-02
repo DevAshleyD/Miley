b71405e1d9f8f86306c97fddf2434f69516177e0
shavit <shavit@users.noreply.github.com>
2017-07-01 20:07:01 -0400

    🛍

--- miley/urls.py
+++ miley/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^accounts/', include('accounts.urls')),
 ]
