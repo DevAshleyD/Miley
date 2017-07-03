811173900f458857b9f34636958b12569e0fc784
shavit <shavit@users.noreply.github.com>
2017-07-03 15:55:11 -0400

    🛍

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .models import Category, Product, Cart
 from .forms import CartAddProductForm
@@ -32,14 +32,14 @@ def cart_add(request, product_id):
         cart.add(product=product,
             quantity=cd['quantity'],
             update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    return redirect('shop:cart_detail')
 
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:cart_detail')
+    return redirect('shop:cart_detail')
 
 def cart_detail(request):
     cart = Cart(request)
@@ -47,4 +47,4 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
             'quantity': item['quantity'],
             'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'shop/cart/detail.html', {'cart': cart})
