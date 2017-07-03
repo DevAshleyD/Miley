fb2b4b422c3cf3dc1add0de0fc07439f9a61a76e
shavit <shavit@users.noreply.github.com>
2017-07-03 12:00:20 -0400

    🛍

--- images/urls.py
+++ images/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
     url(r'^list/$', views.image_list, name='list'),
     url(r'^like/$', views.image_like, name='like'),
+    url(r'^ranking/$', views.image_ranking, name='ranking'),
 ]
--- images/views.py
+++ images/views.py
@@ -80,3 +80,12 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'ok'})
+
+@login_required
+def image_ranking(request):
+    image_ranking = rdis.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images',
+        'most_viewed': most_viewed})
