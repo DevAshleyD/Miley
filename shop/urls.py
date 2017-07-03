0d3e881d32ca66976183251b08e5260363a56dfa
shavit <shavit@users.noreply.github.com>
2017-07-03 15:34:29 -0400

    🛍

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    url(r'^cart/add/(?P<product_id>\d+)$', views.cart_add, name='cart_add'),
+    url(r'^cart/remove/(?P<product_id>\d+)$', views.cart_remove, name='cart_remove'),
 ]
