0c57d17e8da10b29864e5a582da687659a3cf734
shavit <shavit@users.noreply.github.com>
2017-07-05 11:28:07 -0400

    🛍

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,28 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Category, Product, Order, OrderItem
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_may]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
@@ -23,4 +46,5 @@ class OrderAdmin(admin.ModelAdmin):
         'postal_code', 'city', 'created', 'updated', 'paid']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 admin.site.register(Order, OrderAdmin)
