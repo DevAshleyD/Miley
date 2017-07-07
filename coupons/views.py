4cc255eb967ea52e959bc6e8b6bfb749356bffc0
shavit <shavit@users.noreply.github.com>
2017-07-07 11:21:58 -0400

    🛍

--- coupons/views.py
+++ coupons/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from .models import Coupon
-from .ofrms import CouponApplyForm
+from .forms import CouponApplyForm
 
 @require_POST
 def coupon_apply(request):
