cacacf7415bfe082c896f8e54dc2684b78d04320
shavit <shavit@users.noreply.github.com>
2017-08-03 11:17:49 -0400

    🛍

--- miley/urls.py
+++ miley/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
 from .schema import schema
+from .views import homepage
 
 urlpatterns = [
+    url(r'^$', homepage, name='homepage'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^images/', include('images.urls', namespace='images', app_name='images')),
