98d92d49e89eb09e0c79c31b779df2c2ec702f93
shavit <shavit@users.noreply.github.com>
2017-08-03 22:53:42 -0400

    🛍

---
+++ __test__/app.test.js
@@ -0,0 +1,5 @@
+import {t} from '../src/js/helpers/i18n'
+
+test('should translate strings', () => {
+  expect('hello').toBe('hello')
+})
---
+++ src/js/helpers/i18n.js
@@ -0,0 +1,11 @@
+class i18n {
+  constructor(){
+    // TODO: Load remote data
+  }
+
+  T(term){
+    return term
+  }
+}
+
+export const t = (new i18n()).T
