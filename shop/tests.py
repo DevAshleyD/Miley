e2cfda4148754f0cd705b070ae1af30b4bbaf5e1
shavit <shavit@users.noreply.github.com>
2017-08-10 09:23:55 -0400

    🛍

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+class ShopCategoryTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_category(self):
+        """
+        Create a category
+        """
+        category = Category(name='testcategory', slug='testcategory')
+        category.save()
+
+        self.assertNotEqual(category.id, None)
