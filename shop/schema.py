a7a9e578a9cbc57414b50a165c1d7b3c28ab5665
shavit <shavit@users.noreply.github.com>
2017-08-10 17:25:28 -0400

    🛍

--- shop/schema.py
+++ shop/schema.py
@@ -2,12 +2,20 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Category, Product
 
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
 
 class Query(graphene.AbstractType):
+    all_categories = graphene.List(CategoryType)
     all_products = graphene.List(ProductType)
+
+    def resolve_all_categories(self, args, context, info):
+        return Category.objects.select_related('product')
 
     def resolve_all_products(self, args, context, info):
         return Product.objects.all()
