2eed74d86d0f5735fac3c6e7c680ab1a6eb404af
shavit <shavit@users.noreply.github.com>
2021-04-17 06:59:00 -0400

    Minimum required dependencies

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,20 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/auth")]
+fn auth() -> &'static str {
+   "not implemented"
+}
+
+#[get("/auth/<provider>")]
+fn auth_provider(provider: String) -> String {
+    println!("provider: {}", provider);
+
+    "Some provider".to_string()
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, auth, auth_provider])
+        .launch();
 }
