951ed18321e5da686cbf02f9e85e20f6f6d4beef
shavit <shavit@users.noreply.github.com>
2021-04-17 06:39:14 -0400

    Remove legacy framework

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	docker build . -t itstommy/miley
-
-css_watch:
-	bin/css
-
-dev_web:
-	docker-compose up
--- miley/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miley.settings')
-app = Celery('miley', broker=os.environ.get('RABBITMQ_URI', 'amqp://test:test@localhost/miley'))
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
